3e3fdd503ef1d6d54a031ab4b7f41196b75db5fc
Rumon <crash.msr@gmail.com>
2020-12-19 11:15:04 +0600

    Configure database and env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+      retryAttempts: 3,
+      retryDelay: 4444,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
