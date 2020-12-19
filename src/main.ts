7b92979ff8d9cb088b762e8469375b4a1d489b61
Rumon <crash.msr@gmail.com>
2020-12-19 11:25:44 +0600

    Setup Swagger hub

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const docOptions = new DocumentBuilder();
+  docOptions.setTitle('Hotel Management');
+  docOptions.setDescription('ReST API for managing hotels.');
+  docOptions.setVersion('1.0.0');
+
+  const doc = SwaggerModule.createDocument(app, docOptions.build());
+  SwaggerModule.setup('doc', app, doc);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
